4e5b8d9696320ca87429c8aa516c2704c4db5c87
louiskgkim <lkgk31@gmail.com>
2023-02-27 20:38:34 -0800

    adding index seeds file

---
+++ seeds/index.js
@@ -0,0 +1,20 @@
+const seedLiquor = require('./liquor-seeds');
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
+
+  await seedLiquor();
+  console.log('\n----- LIQUORS SEEDED -----\n');
+
+  await seedUsers();
+  console.log('\n----- USERS SEEDED -----\n');
+
+  await seedComments();
+  console.log('\n----- COMMENTS SEEDED -----\n');
+
+  process.exit(0);
+};
+
+seedAll();
