44cf7b6333d209b17572227da2dd4267a1483a77
louiskgkim <lkgk31@gmail.com>
2023-03-02 20:23:21 -0800

    minor changes

--- controllers/inventory-routes.js
+++ controllers/inventory-routes.js
@@ -2,54 +2,53 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Liquor, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
-// still not finished with this
 // need to work on attributes, pulled from my MVC project as this will be a similar process
 
 router.get('/', (req, res) => {
-    Liquor.findAll({
-        where: {
-            user_id: req.session.user_id,
-        },
+  Liquor.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: [
+      // Getting ID, Name, description, date created and liquor ID from Liquor table
+      'id',
+      'name',
+      'description',
+      'date_created',
+      'liquor_id',
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username'],
+      },
+      {
+        model: Comment,
         attributes: [
-            // Getting ID, Name, description, date created and liquor ID from Liquor table
-            'id',
-            'name',
-            'description',
-            'date_created',
-            'liquor_id',
+          'id',
+          'name',
+          'description',
+          'date_created',
+          'liquor_id',
         ],
-        include: [
-            {
-                model: User,
-                attributes: ['username'],
-            },
-            {
-                model: Comment,
-                attributes: [
-                    'id',
-                    'name',
-                    'description',
-                    'date_created',
-                    'liquor_id',
-                ],
-            },
-        ],
-    })
+      },
+    ],
+  })
 
-        .then((dbLiquorData) => {
-            const liquors = dbLiquorData.map((liquor) =>
-                liquor.get({ plain: true })
-            );
-            res.render('homepage', {
-                liquors,
-                loggedIn: req.session.loggedIn,
-            });
-        })
-        // if server error, then return error
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    .then((dbLiquorData) => {
+      const liquors = dbLiquorData.map((liquor) =>
+        liquor.get({ plain: true })
+      );
+      res.render('homepage', {
+        liquors,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    // if server error, then return error
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
