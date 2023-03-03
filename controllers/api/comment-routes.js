4e6cefcd0c0a99eabd14e66d58aa1708e44dbbd9
Avery <averycaldwell7@gmail.com>
2023-03-02 18:19:07 -0800

    update handlebars and images

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -4,48 +4,50 @@ const withAuth = require('../../utils/auth');
 
 // get comments
 router.get('/', (req, res) => {
-  Comment.findAll()
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+    Comment.findAll()
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 // post new comments
 router.post('/', withAuth, (req, res) => {
-  if (req.session) {
-    Comment.create({
-      comment_text: req.body.comment_text,
-      liquor_id: req.body.liquor_id,
-      user_id: req.session.user_id
-    })
-      .then(dbCommentData => res.json(dbCommentData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  }
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            liquor_id: req.body.liquor_id,
+            user_id: req.session.user_id,
+        })
+            .then((dbCommentData) => res.json(dbCommentData))
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
 });
 
 // delete comments
 router.delete('/:id', withAuth, (req, res) => {
-  Comment.destroy({
-    where: {
-      id: req.params.id
-    }
-  })
-    .then(dbCommentData => {
-      if (!dbCommentData) {
-        res.status(404).json({ message: 'No comment found with this id!' });
-        return;
-      }
-      res.json(dbCommentData);
+    Comment.destroy({
+        where: {
+            id: req.params.id,
+        },
     })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+        .then((dbCommentData) => {
+            if (!dbCommentData) {
+                res.status(404).json({
+                    message: 'No comment found with this id!',
+                });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,1 +1,35 @@
 // for future development
+const comments = [
+    {
+        username: 'Michael Scott',
+        message:
+            "I see that you have a great selection of whiskey. Have you tried the Blanton's Single Barrel Bourbon?",
+        user_id: '1',
+    },
+    {
+        username: 'Pam Beasley',
+        message:
+            'I love the variety of spirits you have! Have you ever made a Hemingway Daiquiri with the Havana Club Añejo Blanco?',
+        user_id: '2',
+    },
+    {
+        username: 'Dwight Shrute',
+        message:
+            'You have a nice collection of gin. Have you tried the Botanist Islay Dry Gin? It has a unique flavor profile with 31 botanicals!',
+        user_id: '3',
+    },
+    {
+        username: 'Stanley Hudson',
+        message:
+            'Great selection of tequila! Have you tried the Clase Azul Reposado? It has a smooth taste with hints of vanilla and caramel.',
+        user_id: '4',
+    },
+    {
+        username: 'Jim Halpert',
+        message:
+            'I see you have some bitters in your collection. Have you tried the Bittermens Xocolatl Mole Bitters? They add a nice chocolate and spice flavor to cocktails!',
+        user_id: '5',
+    },
+];
+
+module.exports = comments;
